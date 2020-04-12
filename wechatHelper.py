48a7b3d0deb9dfc90496535df61921b197fd0d4e
ken <1610057945@qq.com>
2020-04-13 07:59:40 +0800

    Added feature:
    Switch to Friday on weekends
    (not completed)

--- wechatHelper.py
+++ wechatHelper.py
@@ -1,6 +1,18 @@
 import itchat,os,sys,datetime
 
 inFolder='grab'
+today=datetime.date.today()
+toweek=today.weekday()
+if toweek in [5,6]:#Saturday, Sunday. Weekday is 0-based. 
+    y,m,d=today.year,today.month,today.day
+    d-=toweek-4 #Calculate how many days from Friday. Again, 0-based. 
+    if d<=0:
+        last_month_days=(datetime.date(y,m,1)-datetime.date(y,m-1,1)).days
+        m-=1
+        d+=last_month_days
+        if m<=0:
+            m+=12
+    today=datetime.date(y,m,d)
 todayFolder=str(datetime.date.today())
 path=os.path.join(sys.path[0],inFolder,todayFolder)
 
