302834c7a301c9de4fcc41d830c8f22f368c08d9
ken <1610057945@qq.com>
2020-04-20 17:21:30 +0800

    convert.py: fixed 2 stupid errors
    1) I pulled the extension guessing mechanism out, and made it
    a function, and forgot that it previously referenced some other
    variables that I didn't pass to the function.
    2) I, again, guessed the file type before downloading the file.

--- convert.py
+++ convert.py
@@ -21,6 +21,7 @@ def download(downfunc,tryTimes=10,fail=lambda *argv,**argvs:notify("Download Fai
 
 def rename(path,name,end):
     fullname=os.path.join(path,name+'.'+end)
+    filename=name+'.'+end
     guess=os.popen('file --extension "%s"'%fullname).read().split(': ')[1].split('/')[0]
     if (not '???' in guess) and (end is not guess):
         os.rename(fullname,os.path.join(path,name+'.'+guess))
@@ -36,13 +37,12 @@ def processFile(filename,path,getter=None,always=False):
     if filename=='': return
     working+=1
     name,end=parseFileName(filename)
-    filename,end=rename(path,name,end)
     if always==True or (end in ['doc','docx','pdf','zip','ppt','pptx','xls','xlsx','mp3','png','jpg','jpeg']):
         if hasattr(getter,'__call__'):
             data=download(getter)
             with open(os.path.join(path,filename),'wb') as f:
                 f.write(data)
-        
+        filename,end=rename(path,name,end)
         if end=='doc' or end=='docx':
             fullname=os.path.join(path,filename)
             os.popen('libreoffice --convert-to odt "%s" --outdir "%s" >> /dev/null'%(fullname,path))
